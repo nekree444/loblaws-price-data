55d8ad60eccc333d88e1f75b62720565f1cfd8c9
nekree444 <nekrre@gmail.com>
2025-01-19 01:29:04 -0500

    added 5 loops per store bump
    to try and get as many items as possible the run could probably take like a day long now

--- step_1.py
+++ step_1.py
@@ -13,7 +13,7 @@ from google.cloud import storage
 
 # Constants
 LISTINGS_NUM = 275
-BUCKET_NAME = os.environ["BUCKET_NAME"]
+BUCKET_NAME = "price-data-storage"
 # KEY_PATH = os.getenv("GCP_SA_KEY")  # Path to your Google Cloud key
 
 # Initialize Google Cloud Storage client
@@ -41,58 +41,56 @@ def get_week():
 
 if __name__ == "__main__":
     print('updated succesffuly')
-    print(BUCKET_NAME)
-    print(bucket)
-
     # for store_id, store_banner in all_stores.items():
     # all_stores = stores_dict()
 
-    # # all_stores = {'1080': 'superstore'} # only do one store for testing
+    all_stores = {'1080': 'superstore'} # only do one store for testing
 
-    # current_week = get_week()
+    current_week = get_week()
 
-    # for store_id, store_banner in all_stores.items():
-    #     print('starting', store_id, store_banner)
-    #     start = time.time()
-    #     csv_data = io.StringIO()
-    #     writer = csv.writer(csv_data)
-    #     writer.writerow(['id', 'brand', 'name', 'price', 'dealPrice'])
-    #     holder = []  # this is for repeat product ids
-    #     continues = 0  # tracks how many times there's an error or empty page; the fifth time we break
-    #     for i in range(1, 500):  # no store has more than 500 pages of products
-    #         errors = 0  # tracks how many times there was an error; if it goes over 3, we break
-    #         while True:  # this loop is for catching errors
-    #             data = get_listings_data(LISTINGS_NUM, i, store_id, store_banner)
-    #             if 'errors' in data:
-    #                 print("Error occurred, retrying...")
-    #                 time.sleep(1)
-    #                 errors += 1
-    #                 if errors > 3:
-    #                     break
-    #             else:
-    #                 break
-    #         if get_product_grid(data):  # if there are products on the page
-    #             for j in data['layout']['sections']['productListingSection']['components'][0]['data']['productGrid']['productTiles']:
-    #                 if j['productId'] in holder:  # checks for duplicates
-    #                     continue
-    #                 if j['pricing']['wasPrice'] is None:  # deal pricing
-    #                     price = j['pricing']['price']
-    #                     dealPrice = None
-    #                 else:
-    #                     price = j['pricing']['wasPrice']
-    #                     dealPrice = j['pricing']['price']
+    for store_id, store_banner in all_stores.items():
+        print('starting', store_id, store_banner)
+        start = time.time()
+        csv_data = io.StringIO()
+        writer = csv.writer(csv_data)
+        writer.writerow(['id', 'brand', 'name', 'price', 'dealPrice'])
+        holder = []  # this is for repeat product ids
+        for _ in range(5):
+            continues = 0  # tracks how many times there's an error or empty page; the fifth time we break
+            for i in range(1, 500):  # no store has more than 500 pages of products
+                errors = 0  # tracks how many times there was an error; if it goes over 3, we break
+                while True:  # this loop is for catching errors
+                    data = get_listings_data(LISTINGS_NUM, i, store_id, store_banner)
+                    if 'errors' in data:
+                        print("Error occurred, retrying...")
+                        time.sleep(1)
+                        errors += 1
+                        if errors > 3:
+                            break
+                    else:
+                        break
+                if get_product_grid(data):  # if there are products on the page
+                    for j in data['layout']['sections']['productListingSection']['components'][0]['data']['productGrid']['productTiles']:
+                        if j['productId'] in holder:  # checks for duplicates
+                            continue
+                        if j['pricing']['wasPrice'] is None:  # deal pricing
+                            price = j['pricing']['price']
+                            dealPrice = None
+                        else:
+                            price = j['pricing']['wasPrice']
+                            dealPrice = j['pricing']['price']
 
-    #                 holder.append(j['productId']) 
-    #                 writer.writerow([j['productId'], j['brand'], j['title'], price, dealPrice])  # write to memory
-    #             # print('wrote', i, 'holder length is', len(holder))
-    #         else:  # if the data is invalid
-    #             # print('continuing...', str(data)[0:30])
-    #             continues += 1
-    #             if continues > 5:  # at 5 continues
-    #                 break  # break if we've reached the max page
-    #             continue  # otherwise, continue
+                        holder.append(j['productId']) 
+                        writer.writerow([j['productId'], j['brand'], j['title'], price, dealPrice])  # write to memory
+                    # print('wrote', i, 'holder length is', len(holder))
+                else:  # if the data is invalid
+                    # print('continuing...', str(data)[0:30])
+                    continues += 1
+                    if continues > 5:  # at 5 continues
+                        break  # break if we've reached the max page
+                    continue  # otherwise, continue
 
-    #     # Upload the CSV data to the Google Cloud Storage bucket
-    #     blob_name = f"listings_{current_week[0]}_{current_week[1]}_{current_week[2]}/{store_banner}/{store_banner}_{store_id}_{current_week[0]}_{current_week[1]}_{current_week[2]}.csv"
-    #     upload_to_bucket(blob_name, csv_data)
-    #     print(f"Time taken for store {store_id} is {time.time() - start} seconds")
+        # Upload the CSV data to the Google Cloud Storage bucket
+        blob_name = f"listings_{current_week[0]}_{current_week[1]}_{current_week[2]}/{store_banner}/{store_banner}_{store_id}_{current_week[0]}_{current_week[1]}_{current_week[2]}.csv"
+        upload_to_bucket(blob_name, csv_data)
+        print(f"Time taken for store {store_id} is {time.time() - start} seconds")
