1af5bb390b72dade290e38ceb7f257f9e3c7c487
nekree444 <nekrre@gmail.com>
2025-01-24 23:11:42 -0500

    dont need tools

--- tools/all_stores_getter.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-def get_all_stores():
-    r = requests.get('https://www.loblaws.ca/api/pickup-locations')
-    return r.json()
-
-if __name__ == '__main__':
-    print(len(get_all_stores()))
