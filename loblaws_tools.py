41f19ea5177fb31b889f2e092ba942d98a62aa5f
nekree444 <nekrre@gmail.com>
2025-01-24 23:06:25 -0500

    switch to search bump
    theyre trying to stop me i think

---
+++ loblaws_tools.py
@@ -0,0 +1,201 @@
+import requests, datetime
+
+def get_all_stores():
+    r = requests.get('https://www.loblaws.ca/api/pickup-locations')
+    return r.json()
+
+def get_product_grid(pages):
+    if not pages:
+        return None
+
+    layout = pages.get('layout')
+    if not layout:
+        return None
+
+    sections = layout.get('sections')
+    if not sections:
+        return None
+
+    product_listing_section = sections.get('productListingSection')
+    if not product_listing_section:
+        return None
+
+    components = product_listing_section.get('components')
+    if not components:
+        return None
+
+    data = components[0].get('data')
+    if not data:
+        return None
+
+    product_grid = data.get('productGrid')
+    if not product_grid:
+        return None
+
+    product_tiles = product_grid.get('productTiles')
+    if not product_tiles:
+        return None
+
+    if product_tiles:
+        return True
+
+def get_product_grid_search(pages):
+    if not pages:
+        return None
+
+    layout = pages.get('layout')
+    if not layout:
+        return None
+
+    sections = layout.get('sections')
+    if not sections:
+        return None
+
+    product_listing_section = sections.get('mainContentCollection')
+    if not product_listing_section:
+        return None
+
+    components = product_listing_section.get('components')
+    if not components:
+        return None
+
+    data = components[0].get('data')
+    if not data:
+        return None
+
+    product_tiles = data.get('productTiles')
+    if not product_tiles:
+        return None
+
+    if product_tiles:
+        return True
+
+def get_listings_data(num_listings, page_num, store_id, store_banner):
+    # Constants
+    API_URL = 'https://api.pcexpress.ca/pcx-bff/api/v2/listingPage/27985'
+    HEADERS = {
+        'User-Agent': 'PC%20Express/202408011329 CFNetwork/1474 Darwin/23.0.0',
+        'Accept-Language': 'en',
+        'x-apikey': 'C1xujSegT5j3ap3yexJjqhOfELwGKYvz',
+        'x-loblaw-tenant-id': 'ONLINE_GROCERIES',
+        'x-application-type': 'Web',
+        # 'x-application-type': 'ios',
+    }
+
+
+    json_data = {
+            'cart': {
+                'cartId': '7e853d0b-15d4-4947-b3f7-204a91491f4e',
+            },
+            'fulfillmentInfo': {
+                'storeId': None,
+                'pickupType': 'STORE',
+                'offerType': 'OG',
+                'date': '01012025',
+                'timeSlot': None,
+            },
+            'listingInfo': {
+                'filters': {},
+                'sort': {},
+                'pagination': {
+                    'from': None,
+                    'size': None,
+                },
+                'includeFiltersInResponse': True,
+            },
+            'banner': None,
+            'userData': {
+                'domainUserId': 'e716d4b1-1b04-4828-9ed8-f28fa19fc34b',
+                'sessionId': '266369e4-c959-4a5d-abd9-fd84707c1a32',
+            },
+        }
+    
+    # script
+
+    json_data['listingInfo']['pagination']['size'] = num_listings
+    json_data['listingInfo']['pagination']['from'] = page_num
+    json_data['fulfillmentInfo']['storeId'] = store_id
+    json_data['banner'] = store_banner
+    # start = time.time()
+    try:
+        r = requests.post(API_URL, headers=HEADERS, json=json_data)
+        return r.json()
+    except:
+        return ['errors']
+
+def get_listings_data_search(num_listings, page_num, store_id, store_banner):
+    # Constants
+    API_URL = 'https://api.pcexpress.ca/pcx-bff/api/v2/products/search'
+    HEADERS = {
+        'User-Agent': 'PC%20Express/202408011329 CFNetwork/1474 Darwin/23.0.0',
+        'Accept-Language': 'en',
+        'x-apikey': 'C1xujSegT5j3ap3yexJjqhOfELwGKYvz',
+        'x-loblaw-tenant-id': 'ONLINE_GROCERIES',
+        'x-application-type': 'Web',
+        # 'x-application-type': 'ios',
+    }
+
+
+    json_data = {
+            'cart': {
+                'cartId': '7e853d0b-15d4-4947-b3f7-204a91491f4e',
+            },
+            'fulfillmentInfo': {
+                'storeId': None,
+                'pickupType': 'STORE',
+                'offerType': 'OG',
+                'date': '01012025',
+                'timeSlot': None,
+            },
+            'listingInfo': {
+                'filters': {
+                    'category': [
+                        '27985',
+                    ],
+                },
+                'pagination': {
+                    'from': None,
+                    'size': None,
+                },
+                'includeFiltersInResponse': True,
+            },
+            'banner': None,
+            'userData': {
+                'domainUserId': 'e716d4b1-1b04-4828-9ed8-f28fa19fc34b',
+                'sessionId': '266369e4-c959-4a5d-abd9-fd84707c1a32',
+            },
+            'searchRelatedInfo': {
+                'term': 'food',
+            },
+        }
+    
+    # script
+
+    json_data['listingInfo']['pagination']['size'] = num_listings
+    json_data['listingInfo']['pagination']['from'] = page_num
+    json_data['fulfillmentInfo']['storeId'] = store_id
+    json_data['banner'] = store_banner
+    # start = time.time()
+    try:
+        r = requests.post(API_URL, headers=HEADERS, json=json_data)
+        return r.json()
+    except:
+        return ['errors']
+
+def upload_to_bucket(blob_name, file_data, bucket):
+    """Uploads a string data as a file to the Google Cloud Storage bucket."""
+    blob = bucket.blob(blob_name)
+    blob.upload_from_string(file_data.getvalue(), content_type='text/csv')
+    print(f"Uploaded {blob_name}")
+
+def stores_dict():
+    temp = get_all_stores()
+    temp = [i for i in temp if i['address']['region'] == 'Ontario']
+    unique_stores = {i['storeId']: i['storeBannerId'] for i in temp}
+    return unique_stores # in format {store_id: store_banner}
+
+def get_week():
+    today = datetime.datetime.today()
+    offset = (today.weekday() - 3) % 7  # 3 corresponds to Thursday
+    last_thursday = today - datetime.timedelta(days=offset)
+    return [last_thursday.year, last_thursday.month, last_thursday.day]
--- step_1.py
+++ step_1.py
@@ -5,49 +5,24 @@ We will do all stores in Ontario
 'nofrills', 'valumart', 'independent', 'loblaw', 'wholesaleclub', 'rapid', 'zehrs', 'fortinos', 'independentcitymarket', 'extrafoods', 'superstore', 'provigo'
 """
 
-from tools.loblaws_tools import get_product_grid
-from tools.get_listings_data import get_listings_data
-from tools.all_stores_getter import get_all_stores
-import time, datetime, csv, io, os
+from loblaws_tools import get_all_stores, get_product_grid, get_listings_data, stores_dict, get_week, upload_to_bucket, get_product_grid_search, get_listings_data_search
+import time, csv, io, os
+from dotenv import load_dotenv
 from google.cloud import storage
+
 
 # Constants
 LISTINGS_NUM = 275
 BUCKET_NAME = "price-data-storage"
-# KEY_PATH = os.getenv("GCP_SA_KEY")  # Path to your Google Cloud key
 
 # Initialize Google Cloud Storage client
+# load_dotenv()
+# KEY_PATH = os.getenv("KEY_PATH")  # Path to your Google Cloud key
 # os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = KEY_PATH
 client = storage.Client()
 bucket = client.bucket(BUCKET_NAME)
 
-def upload_to_bucket(blob_name, file_data):
-    """Uploads a string data as a file to the Google Cloud Storage bucket."""
-    blob = bucket.blob(blob_name)
-    blob.upload_from_string(file_data.getvalue(), content_type='text/csv')
-    print(f"Uploaded {blob_name}")
-
-def stores_dict():
-    temp = get_all_stores()
-    temp = [i for i in temp if i['address']['region'] == 'Ontario']
-    unique_stores = {i['storeId']: i['storeBannerId'] for i in temp}
-    return unique_stores # in format {store_id: store_banner}
-
-def get_week():
-    today = datetime.datetime.today()
-    offset = (today.weekday() - 3) % 7  # 3 corresponds to Thursday
-    last_thursday = today - datetime.timedelta(days=offset)
-    return [last_thursday.year, last_thursday.month, last_thursday.day]
-
-if __name__ == "__main__":
-    print('updated succesffuly')
-    # for store_id, store_banner in all_stores.items():
-    all_stores = stores_dict()
-
-    # all_stores = {'1080': 'superstore'} # only do one store for testing
-
-    current_week = get_week()
-
+def listings_runner(all_stores, current_week):
     for store_id, store_banner in all_stores.items():
         print('starting', store_id, store_banner)
         start = time.time()
@@ -92,5 +67,76 @@ if __name__ == "__main__":
 
         # Upload the CSV data to the Google Cloud Storage bucket
         blob_name = f"listings_{current_week[0]}_{current_week[1]}_{current_week[2]}/{store_banner}/{store_banner}_{store_id}_{current_week[0]}_{current_week[1]}_{current_week[2]}.csv"
-        upload_to_bucket(blob_name, csv_data)
+        upload_to_bucket(blob_name, csv_data, bucket)
         print(f"Time taken for store {store_id} is {time.time() - start} seconds")
+
+def search_runner(all_stores, current_week):
+    for store_id, store_banner in all_stores.items():
+        print('starting', store_id, store_banner)
+        start = time.time()
+        csv_data = io.StringIO()
+        writer = csv.writer(csv_data)
+        writer.writerow(['id', 'brand', 'name', 'price', 'dealPrice'])
+        holder = []  # this is for repeat product ids
+        for _ in range(5):
+            continues = 0  # tracks how many times there's an error or empty page; the fifth time we break
+            for i in range(1, 500):  # no store has more than 500 pages of products
+                errors = 0  # tracks how many times there was an error; if it goes over 3, we break
+                while True:  # this loop is for catching errors
+                    data = get_listings_data_search(LISTINGS_NUM, i, store_id, store_banner)
+                    if 'errors' in data:
+                        print("Error occurred, retrying...")
+                        time.sleep(1)
+                        errors += 1
+                        if errors > 3:
+                            break
+                    else:
+                        break
+                if get_product_grid_search(data):  # if there are products on the page
+                    product_tiles = data['layout']['sections']['mainContentCollection']['components'][0]['data']['productTiles']
+                    product_tiles.extend(data['layout']['sections']['mainContentCollection']['components'][2]['data']['productTiles'])
+                    # data['layout']['sections']['mainContentCollection']['components'][0]['data']['productTiles'].append(data['layout']['sections']['mainContentCollection']['components'][2]['data']['productTiles'])
+                    print(len(product_tiles))
+                    for j in product_tiles:
+                        if j['productId'] in holder:  # checks for duplicates
+                            continue
+                        if j['pricing']['wasPrice'] is None:  # deal pricing
+                            price = j['pricing']['price']
+                            dealPrice = None
+                        else:
+                            price = j['pricing']['wasPrice']
+                            dealPrice = j['pricing']['price']
+
+                        holder.append(j['productId']) 
+                        writer.writerow([j['productId'], j['brand'], j['title'], price, dealPrice])  # write to memory
+                    print('wrote', i, 'holder length is', len(holder))
+                else:  # if the data is invalid
+                    print('continuing...', str(data)[0:30])
+                    continues += 1
+                    if continues > 5:  # at 5 continues
+                        break  # break if we've reached the max page
+                    continue  # otherwise, continue
+
+        # Upload the CSV data to the Google Cloud Storage bucket
+        blob_name = f"listings_{current_week[0]}_{current_week[1]}_{current_week[2]}/{store_banner}/{store_banner}_{store_id}_{current_week[0]}_{current_week[1]}_{current_week[2]}.csv"
+        upload_to_bucket(blob_name, csv_data, bucket)
+        print(f"Time taken for store {store_id} is {time.time() - start} seconds")
+
+def ios_listings_runner():
+    pass
+
+def ios_search_runner():
+    pass
+
+if __name__ == "__main__":
+    print('updated succesffuly')
+    
+    all_stores = stores_dict()
+
+    # all_stores = {'1080': 'superstore'} # only do one store for testing
+
+    current_week = get_week()
+
+    # listings_runner(all_stores, current_week)
+
+    search_runner(all_stores, current_week)
